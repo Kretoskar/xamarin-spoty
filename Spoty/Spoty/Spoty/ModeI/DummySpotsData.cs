e1ba4298684dc17b71360c0a0d12501b6b7f79c6
Oskar <kogutoskar123@gmail.com>
2019-10-12 12:04:08 +0200

    Added dummy database

---
+++ Spoty/Spoty/Spoty/ModeI/DummySpotsData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spoty.ModeI
+{
+    class DummySpotsData
+    {
+        public List<Spot> spots;
+
+        public DummySpotsData()
+        {
+            spots.Add(new Spot("Pomnik mickiewicza", 
+                               new List<SpotCategory>() { SpotCategory.Gaps, SpotCategory.Ledges },
+                               "pomnik_mickiewicza", 
+                               "Schody, krawężniki i murek", 
+                               "Schody 1- 4, nawoskowany krawężnik i marmurowy murek. Nawierzchnia - marmurowe płyty",
+                               50.0374781f,
+                               22.0090554999999f));
+            spots.Add(new Spot("Niebieska siódemka", 
+                               new List<SpotCategory>() { SpotCategory.Gaps },
+                               "niebieska_siodemka", 
+                               "7 schodów, handrail z dużym nachyleniem", 
+                               "7 schodów, najazd z płytek, odjazd na kostkę. Handrail z dużym nachyleniem.",
+                               50.042335f,
+                               21.993306f));
+            spots.Add(new Spot("Handrail Krakowska", 
+                               new List<SpotCategory>() { SpotCategory.Rails },
+                               "handrail_krakowska", 
+                               "Handrail z możliwością naskoku od boku.", 
+                               "Handrail z możliwośćią naskoku od boku.",
+                               50.045358f,
+                               21.983674f));
+            spots.Add(new Spot("Park inwalidów wojennych", 
+                               new List<SpotCategory>() { SpotCategory.Ledges, SpotCategory.Others },
+                               "park_inwalidow_wojennych", 
+                               "Betonowe fale, hubby", 
+                               "Betonowe, wąskie fale. Hubby na obie strony z najazdem z kostki.",
+                               50.022063f,
+                               21.987076f));
+                    
+        }
+    }
+}
---
+++ Spoty/Spoty/Spoty/ModeI/Spot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spoty.ModeI
+{
+    class Spot
+    {
+        private string name;
+        private List<SpotCategory> categories;
+        private string slug;
+        private string shortDesc;
+        private string longDesc;
+        private float latitude;
+        private float longitude;
+
+        public Spot(string name, List<SpotCategory> categories, string slug, string shortDesc, string longDesc, float latitude, float longitude)
+        {
+            this.Name = name;
+            this.Categories = categories;
+            this.Slug = slug;
+            this.ShortDesc = shortDesc;
+            this.LongDesc = longDesc;
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Slug { get => slug; set => slug = value; }
+        public string ShortDesc { get => shortDesc; set => shortDesc = value; }
+        public string LongDesc { get => longDesc; set => longDesc = value; }
+        public float Latitude { get => latitude; set => latitude = value; }
+        public float Longitude { get => longitude; set => longitude = value; }
+        internal List<SpotCategory> Categories { get => categories; set => categories = value; }
+    }
+
+    enum SpotCategory
+    {
+        Gaps,
+        Ledges,
+        Rails,
+        Others
+    }
+}
