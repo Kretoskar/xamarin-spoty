470b8f6b1acf84cc4dec688076b6d07039c9dfdb
Oskar <kogutoskar123@gmail.com>
2019-10-13 11:51:03 +0200

    Added list view selected item handling

---
+++ Spoty/Spoty/Spoty/View/SpotDetailsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Spoty.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SpotDetailsPage : ContentPage
+    {
+        public SpotDetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
