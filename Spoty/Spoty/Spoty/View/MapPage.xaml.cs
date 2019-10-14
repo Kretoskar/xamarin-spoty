53ca0ad374f777b3313d7f72138d3fc248aac134
Oskar <kogutoskar123@gmail.com>
2019-10-14 20:47:11 +0200

    Added map pins

--- Spoty/Spoty/Spoty/View/MapPage.xaml.cs
+++ Spoty/Spoty/Spoty/View/MapPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Spoty.ModeI;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,40 @@ namespace Spoty.View
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasaLocation))
+            {
+                conn.CreateTable<Spot>();
+                var spots = conn.Table<Spot>().ToList();
+                DisplayInMap(spots);
+            }
+        }
+
+        private void DisplayInMap(List<Spot> spots)
+        {
+            foreach (var spot in spots)
+            {
+                try
+                {
+                    var position = new Xamarin.Forms.Maps.Position(spot.Latitude, spot.Longitude);
+
+                    var pin = new Xamarin.Forms.Maps.Pin()
+                    {
+                        Type = Xamarin.Forms.Maps.PinType.SavedPin,
+                        Position = position,
+                        Label = spot.Name
+                    };
+
+                    locationsMap.Pins.Add(pin);
+                }
+                catch (NullReferenceException nre) { }
+                catch (Exception ex) { }
+            }
         }
     }
 }
