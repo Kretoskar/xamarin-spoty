bfcd5fe39a207e0dbc812fb63bd46e776352ce59
Oskar <kogutoskar123@gmail.com>
2019-10-18 23:06:24 +0200

    Added MapVM

--- Spoty/Spoty/Spoty/View/MapPage.xaml.cs
+++ Spoty/Spoty/Spoty/View/MapPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Spoty.ModeI;
+using Spoty.ViewModel;
 using SQLite;
 using System;
 using System.Collections.Generic;
@@ -14,52 +15,21 @@ namespace Spoty.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
-        private const float rzeszowLatitude = 50.041187f;
-        private const float rzeszowLongitude = 21.999121f;
-        private const float mapSpan = .2f;
+        private MapVM viewmodel;
+
         public MapPage()
         {
             InitializeComponent();
+
+            viewmodel = new MapVM(locationsMap);
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
-            DisplayInMap(DummySpotsData.Read());
-            MoveMap();
-        }
-
-        private void DisplayInMap(List<Spot> spots)
-        {
-            foreach (var spot in spots)
-            {
-                try
-                {
-                    var position = new Xamarin.Forms.Maps.Position(spot.Latitude, spot.Longitude);
-
-                    var pin = new Xamarin.Forms.Maps.Pin()
-                    {
-                        Type = Xamarin.Forms.Maps.PinType.SavedPin,
-                        Position = position,
-                        Label = spot.Name
-                    };
-
-                    locationsMap.Pins.Add(pin);
-                }
-                catch (NullReferenceException nre) { }
-                catch (Exception ex) { }
-            }
-        }
-
-        /// <summary>
-        /// Center map on Rzeszów
-        /// </summary>
-        private void MoveMap()
-        {
-            var center = new Xamarin.Forms.Maps.Position(rzeszowLatitude, rzeszowLongitude);
-            var span = new Xamarin.Forms.Maps.MapSpan(center, mapSpan, mapSpan);
-            locationsMap.MoveToRegion(span);
+            viewmodel.DisplayInMap();
+            viewmodel.MoveMap();
         }
     }
 }
