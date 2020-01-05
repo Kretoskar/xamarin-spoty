6357e5579112578a7dfd48cea9cc489d26a86d11
Oskar <kogutoskar123@gmail.com>
2020-01-05 19:44:44 +0100

    Json data, splash screen

--- Spoty/Spoty/Spoty.Android/MainActivity.cs
+++ Spoty/Spoty/Spoty.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using System.IO;
 
 namespace Spoty.Droid
 {
-    [Activity(Label = "Spoty", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
